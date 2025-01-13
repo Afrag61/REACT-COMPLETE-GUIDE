74f1f236ec83ff699eb1ef7aa89f6b7686c3d4c7
Ahmed Frag <a.frag166@gmail.com>
2025-01-13 03:14:11 +0200

    Class-based Components & Context

--- src/components/UserFinder.js
+++ src/components/UserFinder.js
@@ -2,34 +2,33 @@ import { Fragment, useState, useEffect, Component } from "react";
 
 import Users from "./Users";
 import classes from "./UserFinder.module.css";
-
-const DUMMY_USERS = [
-  { id: "u1", name: "Max" },
-  { id: "u2", name: "Manuel" },
-  { id: "u3", name: "Julie" },
-];
+import UsersContext from "../store/users-context";
 
 class UserFinder extends Component {
+  static contextType = UsersContext;
+
   constructor() {
-    super()
+    super();
     this.state = {
       filteredUsers: [],
       searchTerm: "",
     };
   }
 
-  componentDidMount(){
+  componentDidMount() {
     // send http request...
     this.setState({
-      filteredUsers: DUMMY_USERS
-    })
+      filteredUsers: this.context.users,
+    });
   }
 
-  componentDidUpdate(prevProps, prevState){
-    if(prevState.searchTerm !== this.state.searchTerm){
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.searchTerm !== this.state.searchTerm) {
       this.setState({
-        filteredUsers: DUMMY_USERS.filter((user) => user.name.includes(this.state.searchTerm))
-      })
+        filteredUsers: this.context.users.filter((user) =>
+          user.name.includes(this.state.searchTerm)
+        ),
+      });
     }
   }
 
